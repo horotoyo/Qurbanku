55c34282ebebe36e94a9ab76bb95cd8a05a351ff
masuryo <suryowidiyantgm@gmail.com>
2020-04-22 10:22:34 +0700

    add dashboard

---
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -0,0 +1,139 @@
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+  <!-- Sidebar - Brand -->
+  <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+    <div class="sidebar-brand-icon rotate-n-15">
+      <i class="fas fa-cloud-meatball"></i>
+    </div>
+    <div class="sidebar-brand-text mx-3">QurbanKu</div>
+  </a>
+
+  <!-- Divider -->
+  <hr class="sidebar-divider my-0">
+
+  <!-- Nav Item - Dashboard -->
+  <li class="nav-item active">
+    <a class="nav-link" href="index.html">
+      <i class="fas fa-fw fa-home"></i>
+      <span>Dashboard</span></a>
+  </li>
+
+  <!-- Divider -->
+  <hr class="sidebar-divider">
+
+  <!-- Heading -->
+  <div class="sidebar-heading">
+    Pendataan
+  </div>
+
+  <!-- Nav Item - Paket Collapse Menu -->
+  <li class="nav-item">
+    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePaket" aria-expanded="true" aria-controls="collapsePaket">
+      <i class="fa-fw fas fa-layer-group"></i>
+      <span>Pilihan Paket</span>
+    </a>
+    <div id="collapsePaket" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+      <div class="bg-white py-2 collapse-inner rounded">
+        <h6 class="collapse-header">Custom Components:</h6>
+        <a class="collapse-item" href="cards.html">Daftar Paket</a>
+        <a class="collapse-item" href="buttons.html">Tambah Paket</a>
+      </div>
+    </div>
+  </li>
+
+  <!-- Nav Item - Donatur Collapse Menu -->
+  <li class="nav-item">
+    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseDonatur" aria-expanded="true" aria-controls="collapseDonatur">
+      <i class="fa-fw fas fa-users-class"></i>
+      <span>Daftar Donatur</span>
+    </a>
+    <div id="collapseDonatur" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+      <div class="bg-white py-2 collapse-inner rounded">
+        <h6 class="collapse-header">Custom Components:</h6>
+        <a class="collapse-item" href="cards.html">Daftar Donatur</a>
+        <a class="collapse-item" href="buttons.html">Tambah Donatur</a>
+      </div>
+    </div>
+  </li>
+
+  <!-- Nav Item - Data Collapse Menu -->
+  <li class="nav-item">
+    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseData" aria-expanded="true" aria-controls="collapseData">
+      <i class="fas fa-fw fa-database"></i>
+      <span>Data Masuk</span>
+    </a>
+    <div id="collapseData" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
+      <div class="bg-white py-2 collapse-inner rounded">
+        <h6 class="collapse-header">Custom Utilities:</h6>
+        <a class="collapse-item" href="utilities-color.html">Colors</a>
+        <a class="collapse-item" href="utilities-border.html">Borders</a>
+        <a class="collapse-item" href="utilities-animation.html">Animations</a>
+        <a class="collapse-item" href="utilities-other.html">Other</a>
+      </div>
+    </div>
+  </li>
+
+  <!-- Divider -->
+  <hr class="sidebar-divider">
+
+  <!-- Heading -->
+  <div class="sidebar-heading">
+    Pendistribusian
+  </div>
+
+  <!-- Nav Item - Pages Collapse Menu -->
+  <li class="nav-item">
+    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
+      <i class="fas fa-fw fa-truck-moving"></i>
+      <span>Penyerahan</span>
+    </a>
+    <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+      <div class="bg-white py-2 collapse-inner rounded">
+        <a class="collapse-item" href="login.html">Daftar Penerima</a>
+        <a class="collapse-item" href="register.html">Tambah Data</a>
+      </div>
+    </div>
+  </li>
+
+  <!-- Divider -->
+  <hr class="sidebar-divider d-none d-md-block">
+
+    <!-- Heading -->
+  <div class="sidebar-heading">
+    Setting
+  </div>
+
+  <!-- Nav Item - User Collapse Menu -->
+  <li class="nav-item">
+    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUser" aria-expanded="true" aria-controls="collapseUser">
+      <i class="fas fa-fw fa-users"></i>
+      <span>User</span>
+    </a>
+    <div id="collapseUser" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+      <div class="bg-white py-2 collapse-inner rounded">
+        <a class="collapse-item" href="login.html">Daftar User</a>
+        <a class="collapse-item" href="register.html">Tambah User</a>
+      </div>
+    </div>
+  </li>
+
+  <!-- Nav Item - Role Collapse Menu -->
+  <li class="nav-item">
+    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseRole" aria-expanded="true" aria-controls="collapseRole">
+      <i class="fas fa-fw fa-users-cog"></i>
+      <span>Role</span>
+    </a>
+    <div id="collapseRole" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+      <div class="bg-white py-2 collapse-inner rounded">
+        <a class="collapse-item" href="login.html">Daftar Role</a>
+        <a class="collapse-item" href="register.html">Tambah Role</a>
+      </div>
+    </div>
+  </li>
+
+  <!-- Sidebar Toggler (Sidebar) -->
+  <div class="text-center d-none d-md-inline">
+    <button class="rounded-circle border-0" id="sidebarToggle"></button>
+  </div>
+
+</ul>
